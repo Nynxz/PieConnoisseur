9ca2f53c1fa5b62c4613a2703387c4f995eb05ed
Henry Lee <nynxzx@gmail.com>
2021-03-11 00:54:28 +1000

    Initial Commit

---
+++ PieConnoisseur/PieConnoisseurApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieConnoisseurApp.swift
+//  PieConnoisseur
+//
+//  Created by Henry Lee on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct PieConnoisseurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
