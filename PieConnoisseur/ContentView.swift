a43bb39105e0388acf4bef6e7ddca7b5b0407bc0
Henry Lee <nynxzx@gmail.com>
2021-03-11 20:16:06 +1000

    Edit Content View - Boilerplate hardcode

--- PieConnoisseur/ContentView.swift
+++ PieConnoisseur/ContentView.swift
@@ -8,34 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        NavigationView {
-            List {
-                ListItemView()
-                ListItemView()
-                ListItemView()
-                ListItemView()
-            }
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar{
-                ToolbarItem(placement: .navigationBarLeading){
-                        Text("Pie Connoisseur")
-                            .font(.title)
-                            .bold()
-                            .multilineTextAlignment(.leading)
-                            
-                }
-                ToolbarItem(placement: .navigationBarTrailing){
-                        Image(systemName: "gearshape")
-                }
+        
+        let data = loadPieData()
+        ScrollView{
+            LazyVStack{
+                ListItemView(pie: data[0])
+                ListItemView(pie: data[0])
+                ListItemView(pie: data[0])
+                ListItemView(pie: data[0])
+                ListItemView(pie: data[0])
+                ListItemView(pie: data[0])
+                ListItemView(pie: data[0])
+                ListItemView(pie: data[0])
+                ListItemView(pie: data[0])
             }
         }
+
+
+
+//        NavigationView {
+//            List {
+//                ListItemView()
+//                ListItemView()
+//                ListItemView()
+//                ListItemView()
+//            }
+//            .navigationBarTitleDisplayMode(.inline)
+//            .toolbar{
+//                ToolbarItem(placement: .navigationBarLeading){
+//                        Text("Pie Connoisseur")
+//                            .font(.title)
+//                            .bold()
+//                            .multilineTextAlignment(.leading)
+//
+//                }
+//                ToolbarItem(placement: .navigationBarTrailing){
+//                        Image(systemName: "gearshape")
+//                }
+//            }
+//        }
     }
 
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        
         ContentView()
     }
 }
