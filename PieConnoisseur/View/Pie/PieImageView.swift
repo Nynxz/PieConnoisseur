2ef3d893f6167129c5fdfef5067c3f0a6c6fc691
Henry Lee <nynxzx@gmail.com>
2021-03-11 20:16:35 +1000

    Edit Pie Image View to be Dynamic

--- PieConnoisseur/View/Pie/PieImageView.swift
+++ PieConnoisseur/View/Pie/PieImageView.swift
@@ -8,19 +8,20 @@
 import SwiftUI
 
 struct PieImageView: View {
+    var image: Image
     var body: some View {
-        Image("stone_artisan_pie")
-            .resizable()
-            .frame(width: 200, height: 200, alignment: .center)
-            .clipShape(Circle())
-            .overlay(Circle().stroke(Color.white,  lineWidth: 5))
-            .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 10)
-            .padding(20)
+                image
+                    .resizable()
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white,  lineWidth: 5))
+                    .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 10)
+                    .padding(20)
     }
 }
 
 struct PieImageView_Previews: PreviewProvider {
     static var previews: some View {
-        PieImageView()
+        PieImageView(image: Image(loadPieData()[0].image))
     }
 }
